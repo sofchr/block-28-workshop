c55656a354b9e2fa36601de9a425f7c93af1b590
Sofie Christensen <sofiec510@gmail.com>
2023-08-15 14:12:49 -0400

    first and second tab

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <div id="container">
+        <div id="navbar">{/* navigation here */}</div>
+        <div id="main-section">
+          <Routes>
+            <Route path="/" element={<h1>Home</h1>} />
+            <Route path="/blue" element={<h1>Blue</h1>} />
+            <Route path="/red" element={<h1>Red</h1>} />
+          </Routes>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
