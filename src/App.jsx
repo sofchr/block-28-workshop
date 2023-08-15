d85e6cf467fbed8b09e807e1e2cfb0885af5b633
Sofie Christensen <sofiec510@gmail.com>
2023-08-15 14:32:51 -0400

    Home component, styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { Routes, Route, Link } from "react-router-dom";
 import Red from "./components/Red";
 import Blue from "./components/Blue";
+import Home from "./components/Home";
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
         </div>
         <div id="main-section">
           <Routes>
-            <Route path="/" element={<h1>Home</h1>} />
+            <Route path="/" element={Home()} />
             <Route path="/blue" element={Blue()} />
             <Route path="/red" element={Red()} />
           </Routes>
