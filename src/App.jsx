12c718bd611847c561299d897f20d0d90c11dcbe
Sofie Christensen <sofiec510@gmail.com>
2023-08-15 14:24:08 -0400

    Blue and Red components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import { useState } from "react";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+import Red from "./components/Red";
+import Blue from "./components/Blue";
 
 function App() {
   return (
     <>
       <div id="container">
-        <div id="navbar">{/* navigation here */}</div>
+        <div id="navbar">
+          <Link to="/">Home</Link>
+          <Link to="/blue">Blue</Link>
+          <Link to="/red">Red</Link>
+        </div>
         <div id="main-section">
           <Routes>
             <Route path="/" element={<h1>Home</h1>} />
-            <Route path="/blue" element={<h1>Blue</h1>} />
-            <Route path="/red" element={<h1>Red</h1>} />
+            <Route path="/blue" element={Blue()} />
+            <Route path="/red" element={Red()} />
           </Routes>
         </div>
       </div>
